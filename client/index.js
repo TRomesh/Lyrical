bfe58f4868553a9a74f36803024b1cac0c07297a
Tharaka Romesh <tharakaromesh@gmail.com>
2017-12-23 20:44:29 +0530

    Fetch songs from mongodb trough apollo-grapghql with react

--- client/index.js
+++ client/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
-import { render } from 'react-dom';
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { render } from "react-dom";
+import SongList from "./components/SongList";
 
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:4000' }),
-  cache: new InMemoryCache(),
+  link: createHttpLink({ uri: "http://localhost:4000/graphql" }),
+  cache: new InMemoryCache()
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
