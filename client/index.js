9e10ac24dff27b2213dd729706fb51aaf4a3e304
Tharaka Romesh <tharakaromesh@gmail.com>
2017-12-23 18:47:06 +0530

    Configure to latest Apollo version(v2)

--- client/index.js
+++ client/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { render } from 'react-dom';
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
